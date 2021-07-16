8a3422f5757e932924db6dea2ccfa0256692a69d
EmilSejerP <71255873+EmilSejerP@users.noreply.github.com>
2021-07-16 07:18:32 +0200

    month layout with TextInput and Button generated by for loop

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from TimeScript import *
-
+from datetime import date
 class TestLayout(BoxLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         time_script = TimeScript()
-        layout = StackLayout()
-        elements = BoxLayout(orientation="vertical",size_hint=[1,0.8],pos_hint=["bottom","bottom"])
-        layout.add_widget(elements)
-        elements.add_widget(Button(text="button1"))
-        elements.add_widget(Button(text="button2"))
-        self.add_widget(layout)
+        grid_layout = GridLayout(cols=7)
+
+        for i in range(time_script.get_day_amount()):
+            day = BoxLayout(orientation="vertical")
+            day.add_widget(TextInput(text=f"{str(time_script.get_current_time().year)} {str(time_script.get_current_time().month)} {i+1}"))
+            day.add_widget(Button(text="Button1"))
+            grid_layout.add_widget(day)
+        self.add_widget(grid_layout)
+
     pass
 
 
